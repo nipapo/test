cdbf30f52c50d7655599b1e37fafac22449c5083
nipapo <mitsu1897@gmail.com>
2023-03-15 01:56:14 +0900

    vsformac

---
+++ VSforMac/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//static void Main()
+// {
+
+
+//     string str1,str2;
+//     Console.Write("str1=");
+//     str1 = Console.ReadLine();
+//     Console.Write("str2=");
+//     str2 = Console.ReadLine();
+
+//     int i;
+//     i = int.Parse(str1);
+
+//     Console.WriteLine("str1 = {0}",str1+str2);
+//     Console.WriteLine("str1 = {0}",i);
+// }
+
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("aaa");
+        }
+    }
+}
+
+//          123456"9999asdf"
+//          123456"9999asdf"
+//          123456"9999asdf"
+//          123456"9999asdf"
+//          123456"9999asdf"
+//          123456"9999asdf"
+//          123456"9999asdf"
+//          123456"9999asdf"
+
+
---
+++ VSforMac/cs.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a,b,d;
+            int c;
+            a = 1.23;
+            b = (double)12;
+            c = (int)1.23;
+            d = 10;
+            Console.WriteLine("a={0} b={1} c={2} d={3}",a,b,c,d);
+        }
+    }
+}
